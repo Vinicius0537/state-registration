8548eca19a2fccbc290bcaa0f7b1addc81cc6436
Vinicius0537 <pereiravinicius0537@gmail.com>
2023-07-18 10:23:25 -0300

    Summation function to calculate the verify digit added

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+// Creating summation function to calculate the 
+// verify digit
+
+int summation(long long registration, int digit, 
+int multiplier, int qtydigits) {
+	
+	int sum = 0;
+		
+	while(registration > 0) {
+		
+		digit = registration % 10;
+		registration /= 10;
+			
+		if(multiplier > 9) {
+			multiplier = 2;
+		}
+		sum += (digit * multiplier);
+		multiplier++;
+	}
+	return sum;
+}
+
 int main() {
 	return 0;
 }
