4337af294073a7b7be0a434218b7e029b8d0a5c2
Vinicius0537 <pereiravinicius0537@gmail.com>
2023-07-18 10:27:24 -0300

    Getting the two last digits

--- src/main.cpp
+++ src/main.cpp
@@ -25,5 +25,30 @@ int multiplier, int qtydigits) {
 }
 
 int main() {
+	
+	long long registration;
+	int digit, digit_1, digit_2;
+	int qtydigits;
+	
+	cout << "\n";	
+	cout << "Enter a state registration: ";
+	cin >> registration;
+	
+	qtydigits = 0;
+	
+	// Getting the two last digits
+	while(qtydigits < 2) {
+		digit = registration % 10;
+		registration /= 10;
+		if(qtydigits == 0) {
+			digit_2  = digit;
+		}
+		else if(qtydigits == 1) {
+			digit_1 = digit;
+		}
+		
+		qtydigits++;
+	}
+	
 	return 0;
 }
