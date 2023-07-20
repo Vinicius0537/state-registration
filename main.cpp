4d68854323c5b348cf7e3c294c689880fe8cc843
Vinicius Pereira <52641307+Vinicius0537@users.noreply.github.com>
2023-07-20 20:58:42 -0300

    Creating function to verify if the state registration is valid

--- main.cpp
+++ main.cpp
@@ -24,84 +24,101 @@ int multiplier, int qtydigits) {
 	return sum;
 }
 
+// Creating function to verify if the 
+// state registration is valid
+
+bool ValidateStateRegistration(long long registration) {
+    int digit, digit_1, digit_2;
+    int qtydigits, sum, multiplier;
+    int rest, result, VerifyDigit_1;
+    int VerifyDigit_2;
+    bool isValid;
+
+    qtydigits = 0;
+	
+    // Getting the two last digits
+    while(qtydigits < 2) {
+        digit = registration % 10;
+	registration /= 10;
+	if(qtydigits == 0) {
+	    digit_2  = digit;
+	}
+	else if(qtydigits == 1) {
+	    digit_1 = digit;
+	}
+		
+	qtydigits++;
+    }
+	
+    qtydigits = 0;
+    sum = 0;
+    multiplier = 2;
+	
+    // Calculating the first verify digit
+	
+    sum = summation(registration, digit, multiplier, 
+    qtydigits);
+	
+    rest = sum % 11;
+    result = 11 - rest;
+	
+    if(result == 10 || result == 11) {
+        VerifyDigit_1 = 0;
+    }
+    else {
+        VerifyDigit_1 = result;
+    }
+
+    // Calculating the second verify digit
+	
+    multiplier = 3;
+	
+    sum = 0;
+	
+    sum = (VerifyDigit_1 * 2) + summation(registration, 
+    digit, multiplier, qtydigits);
+	
+    rest = sum % 11;
+    result = 11 - rest;
+	
+    if(result == 10 || result == 11) {
+        VerifyDigit_2 = 0;
+    }
+    else {
+        VerifyDigit_2 = result;
+    }
+	
+    // Checking if the state registration is valid 
+    // or not
+	
+    cout << "\n";
+	
+    if(VerifyDigit_1 == digit_1) {
+        if(VerifyDigit_2 == digit_2) {
+	    isValid = true;
+	}
+    }
+    else {
+        isValid = false;
+    }
+    return isValid;
+}
+
 int main() {
 	
 	long long registration;
-	int digit, digit_1, digit_2;
-	int qtydigits, sum, multiplier;
-	int rest, result, VerifyDigit_1;
-	int VerifyDigit_2;
 	
 	cout << "\n";	
 	cout << "Enter a state registration: ";
 	cin >> registration;
-	
-	qtydigits = 0;
-	
-	// Getting the two last digits
-	while(qtydigits < 2) {
-		digit = registration % 10;
-		registration /= 10;
-		if(qtydigits == 0) {
-			digit_2  = digit;
-		}
-		else if(qtydigits == 1) {
-			digit_1 = digit;
-		}
-		
-		qtydigits++;
-	}
-	
-	qtydigits = 0;
-	sum = 0;
-	multiplier = 2;
-	
-	// Calculating the first verify digit
-	
-	sum = summation(registration, digit, multiplier, 
-	qtydigits);
-	
-	rest = sum % 11;
-	result = 11 - rest;
-	
-	if(result == 10 || result == 11) {
-		VerifyDigit_1 = 0;
-	}
-	else {
-		VerifyDigit_1 = result;
-	}
-	
-	// Calculating the second verify digit
-	
-	multiplier = 3;
-	
-	sum = 0;
-	
-	sum = (VerifyDigit_1 * 2) + summation(registration, 
-	digit, multiplier, qtydigits);
-	
-	rest = sum % 11;
-	result = 11 - rest;
-	
-	if(result == 10 || result == 11) {
-		VerifyDigit_2 = 0;
-	}
-	else {
-		VerifyDigit_2 = result;
-	}
-	
-	// Checking if the state registration is valid 
-	// or not
-	
+
 	cout << "\n";
-	
-	if(VerifyDigit_1 == digit_1) {
-		if(VerifyDigit_2 == digit_2) {
-			cout << "The state registration is valid!";
-		}
+
+	if(ValidateStateRegistration(registration) == true) {
+	    cout << "the state registration is valid!";
 	}
 	else {
-		cout << "The state registration is invalid!";
+	    cout << "the state registration is not valid!";
 	}
 	
 	cout << "\n\n";
